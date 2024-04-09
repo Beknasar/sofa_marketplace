288374a25038ed1ee08ddc445ce0e913a3c354a8
beknazar <680633@gmail.com>
2024-04-09 21:59:25 +0600

    Модельки категории и комнат зарегистрированы в админке

--- source/webapp/admin.py
+++ source/webapp/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
+from webapp.models import Category, Room
 
-# Register your models here.
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+    list_filter = ['rooms']
+
+
+class RoomAdmin(admin.ModelAdmin):
+    list_display = ['pk', 'name']
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Room, RoomAdmin)
--- source/webapp/models.py
+++ source/webapp/models.py
@@ -14,6 +14,7 @@ class Room(models.Model):
 
 class Category(models.Model):
     name = models.CharField(max_length=50, null=False, blank=False, verbose_name='Название категории мебели')
+    rooms = models.ManyToManyField(Room, verbose_name='Категории мебели', related_name='categories')
 
     def __str__(self):
         return self.name
