a9424c76b1c028c29e5da706712c6ef35916e49a
Beknazar <680633@gmail.com>
2024-06-06 15:11:28 +0600

    Поменял локализацию на ru

--- source/main/settings/base.py
+++ source/main/settings/base.py
@@ -80,14 +80,17 @@ PASSWORD_HASHERS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'ru'
 
 TIME_ZONE = 'Asia/Bishkek'
 
 USE_I18N = True
-
+USE_L10N = True
 USE_TZ = True
 
+LOCALE_PATHS = [
+    os.path.join(BASE_DIR, 'locale')
+]
 
 STATIC_URL = '/static/'
 
